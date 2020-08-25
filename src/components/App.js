76d673ae1481514ba7a289e2ad258a518b0249a7
GBMan <csablons@gmail.com>
2020-08-25 11:45:21 +0200

    Petite optimisation sur la home. Ajout de l'exercice sur les Doubles.

--- src/components/App.js
+++ src/components/App.js
@@ -3,37 +3,33 @@ import '../scss/app.scss'
 import ComplementTen from './exercices/ComplementTen'
 import Multiplication from './exercices/Multiplication'
 import Invariables from './exercices/Invariables'
+import Double from './exercices/Double'
 
 const LEVEL_CP = "cp"
+const LEVEL_CP_DOUBLE = "cpdouble"
 const LEVEL_CE2 = "ce2"
 const LEVEL_CE2_INVARIABLES = "ce2i"
 
 function App() {
   const [level, setLevel] = useState(LEVEL_CE2_INVARIABLES)
 
-  function handleClickCP(event) {
-    setLevel(LEVEL_CP)
-  }
-
-  function handleClickCE2(event) {
-    setLevel(LEVEL_CE2)
-  }
-
-  function handleClickCE2Invariables(event) {
-    setLevel(LEVEL_CE2_INVARIABLES)
+  function handleClickChoice(level) {
+    setLevel(level)
   }
 
   return (
     <div className="app">
       <header className="app--header">
-        <button className="btn app--header-btn" onClick={handleClickCP}>CP - Complément à 10</button>
-        <button className="btn app--header-btn" onClick={handleClickCE2}>CE2 - Multiplication</button>
-        <button className="btn app--header-btn" onClick={handleClickCE2Invariables}>CE2 - Mots invariables</button>
+        <button className="btn app--header-btn" onClick={(event) => handleClickChoice(LEVEL_CP)}>CP - Complément à 10</button>
+        <button className="btn app--header-btn" onClick={(event) => handleClickChoice(LEVEL_CP_DOUBLE)}>CP - Double</button>
+        <button className="btn app--header-btn" onClick={(event) => handleClickChoice(LEVEL_CE2_INVARIABLES)}>CE2 - Mots invariables</button>
+        <button className="btn app--header-btn" onClick={(event) => handleClickChoice(LEVEL_CE2)}>CE2 - Multiplication</button>
       </header>
       <main className="app--main">
         {level === LEVEL_CP && <ComplementTen />}
-        {level === LEVEL_CE2 && <Multiplication />}
+        {level === LEVEL_CP_DOUBLE && <Double />}
         {level === LEVEL_CE2_INVARIABLES && <Invariables />}
+        {level === LEVEL_CE2 && <Multiplication />}
       </main>
       <footer className="app--footer">
         Développé par <a href="https://sablons.fr" className="app--footer-link" target="_blank">Charles</a>
---
+++ src/components/exercices/Double.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { getRandom_1_to_9, convertMillisecondsToMinutesSeconds } from '../../utils/misc'
+import { FORMAT } from '../../utils/const'
+import QuizzTemplate from './QuizzTemplate'
+
+const LOCAL_STORAGE_KEY = "exercices-cp"
+
+export default function Double() {
+    const title = "Double"
+    const nbQuestions = 10
+    const rules = `Tu dois donner le double du nombre indiqué. Il y aura ${nbQuestions} questions.`
+    const format = FORMAT.NUMBER
+
+    function getQuestion() {
+        const number = getRandom_1_to_9()
+        const question = `Le double de ${number} est ?`
+        const answer = number * 2
+        return {question: question, answer:answer}
+    }
+    function getBilan(timerMs, nbErrors, highScore) {
+        let score = 5
+
+        if (nbErrors > 0) score-- 
+        if (nbErrors > 1) score--
+        if (timerMs > nbQuestions*3000) score-- 
+        if (timerMs > nbQuestions*6000) score--
+
+        let starScore = ""
+        for (let i = 0; i < 5; i++) {
+            if (i < score) starScore += "Z"
+            else starScore += "b"
+        }
+
+        return `Bravo ! Tu as terminé en <strong>${convertMillisecondsToMinutesSeconds(timerMs)}</strong> avec <strong>${nbErrors === 0 ? "aucune erreur" : (nbErrors === 1) ? "1 erreur" : nbErrors+" erreurs"}</strong>. Cela fait à peu près <strong>${Math.round((timerMs/1000)/nbQuestions)}</strong> secondes par question.<br />
+        <span class="exercice--bilan-score">${starScore}</span><br />
+        Le record est <strong>${convertMillisecondsToMinutesSeconds(highScore)}</strong>.`
+    }
+
+    return (
+        <QuizzTemplate title={title} getQuestion={getQuestion} getBilan={getBilan} nbQuestions={nbQuestions} rules={rules} localStorageKey={LOCAL_STORAGE_KEY} format={format} />
+    )
+}
